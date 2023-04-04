f205573f8db8b9b839a7a1f46c000e6efa16947d
Akbar <zolotojznak@gmail.com>
2023-04-04 15:17:13 +0500

    translation23

--- root/visola_blazer/static/js/google-translate.js
+++ root/visola_blazer/static/js/google-translate.js
@@ -1,42 +1,40 @@
-const googleTranslateConfig = {
-    lang: "ru",
-    userAgent: false,
-  };
-  
-  function TranslateInit() {
-    let code = TranslateGetCode();
-    $('[data-google-lang="' + code + '"]').addClass('language__img_active');
-    if (code == googleTranslateConfig.lang) {
-      TranslateClearCookie();
-    }
-    new google.translate.TranslateElement({
-      pageLanguage: googleTranslateConfig.lang,
-    });
-    $('[data-google-lang]').click(function () {
-      TranslateSetCookie($(this).attr("data-google-lang"));
-      window.location.reload();
-    });
-  }
-  
-  function TranslateGetCode() {
-    let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
-    return lang.substr(-2);
-  }
-  
-  function TranslateClearCookie() {
-    $.cookie('googtrans', null);
-    $.cookie("googtrans", null, {
-      domain: "." + document.domain,
-    });
-  }
-  
-  function TranslateSetCookie(code) {
-    $.cookie('googtrans', "/auto/" + code);
-    $.cookie("googtrans", "/auto/" + code, {
-      domain: "." + document.domain,
-    });
-  }
-  
-  $('.language__img').click(function () {
-    TranslateInit();
-  });
+$('.language__img').click(function () {
+    const googleTranslateConfig = {
+        lang: "ru",
+        userAgent: false,
+      };
+      
+      function TranslateInit() {
+        let code = TranslateGetCode();
+        $('[data-google-lang="' + code + '"]').addClass('language__img_active');
+        if (code == googleTranslateConfig.lang) {
+          TranslateClearCookie();
+        }
+        new google.translate.TranslateElement({
+          pageLanguage: googleTranslateConfig.lang,
+        });
+        $('[data-google-lang]').click(function () {
+          TranslateSetCookie($(this).attr("data-google-lang"));
+          window.location.reload();
+        });
+      }
+      
+      function TranslateGetCode() {
+        let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
+        return lang.substr(-2);
+      }
+      
+      function TranslateClearCookie() {
+        $.cookie('googtrans', null);
+        $.cookie("googtrans", null, {
+          domain: "." + document.domain,
+        });
+      }
+      
+      function TranslateSetCookie(code) {
+        $.cookie('googtrans', "/auto/" + code);
+        $.cookie("googtrans", "/auto/" + code, {
+          domain: "." + document.domain,
+        });
+      }
+});
