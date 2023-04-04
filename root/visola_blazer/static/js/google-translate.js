1694a4d014554e87faef074c4211740dc265796b
Akbar <zolotojznak@gmail.com>
2023-04-04 14:15:18 +0500

    translation6

--- root/visola_blazer/static/js/google-translate.js
+++ root/visola_blazer/static/js/google-translate.js
@@ -1,24 +1,14 @@
-const russianFlag = document.getElementById('russian-flag');
-const englishFlag = document.getElementById('english-flag');
-const uzbekFlag = document.getElementById('uzbek-flag');
+function translatePage(lang) {
+    // Выбор элемента для перевода
+    var translateContainer = document.getElementById('page-content');
+    // Установка языка для перевода
+    var languageCode = lang;
+    // Вызов функции Google Translate
+    new google.translate.TranslateElement({pageLanguage: 'ru', includedLanguages: languageCode, layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'page-content');
+  }
 
-russianFlag.addEventListener('click', () => {
-  // Устанавливаем язык страницы на русский
-  document.documentElement.lang = 'ru';
-  // Переводим страницу на русский язык с помощью Google Translate
-  googleTranslateElementInit();
-});
 
-englishFlag.addEventListener('click', () => {
-  // Устанавливаем язык страницы на английский
-  document.documentElement.lang = 'en';
-  // Переводим страницу на английский язык с помощью Google Translate
-  googleTranslateElementInit();
-});
-
-uzbekFlag.addEventListener('click', () => {
-    // Устанавливаем язык страницы на английский
-    document.documentElement.lang = 'uz';
-    // Переводим страницу на английский язык с помощью Google Translate
-    googleTranslateElementInit();
-});
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'ru', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+}
+  
