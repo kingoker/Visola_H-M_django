f4df13be8192bb01c6f11d44b684dcf3420fbb56
Akbar <zolotojznak@gmail.com>
2023-04-04 15:05:51 +0500

    translation20

--- root/visola_blazer/static/js/google-translate.js
+++ root/visola_blazer/static/js/google-translate.js
@@ -1,51 +1,38 @@
-TranslateClearCookie();
-
 const googleTranslateConfig = {
     lang: "ru",
-};
-
-function TranslateInit() {
-
+    userAgent: false,
+  };
+  
+  function TranslateInit() {
     let code = TranslateGetCode();
-    // Находим флаг с выбранным языком для перевода и добавляем к нему активный класс
     $('[data-google-lang="' + code + '"]').addClass('language__img_active');
-
     if (code == googleTranslateConfig.lang) {
-        // Если язык по умолчанию, совпадает с языком на который переводим
-        // То очищаем куки
-        TranslateClearCookie();
+      TranslateClearCookie();
     }
-
-    // Инициализируем виджет с языком по умолчанию
     new google.translate.TranslateElement({
-        pageLanguage: googleTranslateConfig.lang,
+      pageLanguage: googleTranslateConfig.lang,
     });
-
-    // Вешаем событие  клик на флаги
     $('[data-google-lang]').click(function () {
-        TranslateSetCookie($(this).attr("data-google-lang"))
-        // Перезагружаем страницу
-        window.location.reload();
+      TranslateSetCookie($(this).attr("data-google-lang"));
+      window.location.reload();
     });
-}
-
-function TranslateGetCode() {
-    // Если куки нет, то передаем дефолтный язык
+  }
+  
+  function TranslateGetCode() {
     let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
     return lang.substr(-2);
-}
-
-function TranslateClearCookie() {
+  }
+  
+  function TranslateClearCookie() {
     $.cookie('googtrans', null);
     $.cookie("googtrans", null, {
-        domain: "." + document.domain,
+      domain: "." + document.domain,
     });
-}
-
-function TranslateSetCookie(code) {
-    // Записываем куки /язык_который_переводим/язык_на_который_переводим
+  }
+  
+  function TranslateSetCookie(code) {
     $.cookie('googtrans', "/auto/" + code);
     $.cookie("googtrans", "/auto/" + code, {
-        domain: "." + document.domain,
+      domain: "." + document.domain,
     });
-}
+  }
