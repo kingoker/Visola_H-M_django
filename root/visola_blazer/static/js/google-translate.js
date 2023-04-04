6597b66654a5b9cefe8915aef49abc3e4005db52
Akbar <zolotojznak@gmail.com>
2023-04-04 15:34:20 +0500

    translation27

--- root/visola_blazer/static/js/google-translate.js
+++ root/visola_blazer/static/js/google-translate.js
@@ -1,60 +1,51 @@
-window.onload = () => {
-    googleTranslateElementInit();
-  };
-
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'ru', includedLanguages: 'en,ru', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-}
-  
-document.querySelectorAll('img[data-lang]').forEach(flag => {
-flag.addEventListener('click', () => {
-    const lang = flag.dataset.lang;
-    const googleTranslateElement = document.querySelector('.goog-te-banner-frame');
-    if (googleTranslateElement) {
-    googleTranslateElement.style.display = 'none';
+$('.language__img').click(function () {
+    const googleTranslateConfig = {
+        lang: "ru",
+    };
+    
+    function TranslateInit() {
+    
+        let code = TranslateGetCode();
+        // Находим флаг с выбранным языком для перевода и добавляем к нему активный класс
+        $('[data-google-lang="' + code + '"]').addClass('language__img_active');
+    
+        if (code == googleTranslateConfig.lang) {
+            // Если язык по умолчанию, совпадает с языком на который переводим
+            // То очищаем куки
+            TranslateClearCookie();
+        }
+    
+        // Инициализируем виджет с языком по умолчанию
+        new google.translate.TranslateElement({
+            pageLanguage: googleTranslateConfig.lang,
+        });
+    
+        // Вешаем событие  клик на флаги
+        $('[data-google-lang]').click(function () {
+            TranslateSetCookie($(this).attr("data-google-lang"))
+            // Перезагружаем страницу
+            window.location.reload();
+        });
     }
-    new google.translate.TranslateElement({pageLanguage: 'ru', includedLanguages: lang, layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+    
+    function TranslateGetCode() {
+        // Если куки нет, то передаем дефолтный язык
+        let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
+        return lang.substr(-2);
+    }
+    
+    function TranslateClearCookie() {
+        $.cookie('googtrans', null);
+        $.cookie("googtrans", null, {
+            domain: "." + document.domain,
+        });
+    }
+    
+    function TranslateSetCookie(code) {
+        // Записываем куки /язык_который_переводим/язык_на_который_переводим
+        $.cookie('googtrans', "/auto/" + code);
+        $.cookie("googtrans", "/auto/" + code, {
+            domain: "." + document.domain,
+        });
+    }
 });
-});
-  
-
-// $('.language__img').click(function () {
-//     const googleTranslateConfig = {
-//         lang: "ru",
-//         userAgent: false,
-//       };
-      
-//       function TranslateInit() {
-//         let code = TranslateGetCode();
-//         $('[data-google-lang="' + code + '"]').addClass('language__img_active');
-//         if (code == googleTranslateConfig.lang) {
-//           TranslateClearCookie();
-//         }
-//         new google.translate.TranslateElement({
-//           pageLanguage: googleTranslateConfig.lang,
-//         });
-//         $('[data-google-lang]').click(function () {
-//           TranslateSetCookie($(this).attr("data-google-lang"));
-//           window.location.reload();
-//         });
-//       }
-      
-//       function TranslateGetCode() {
-//         let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
-//         return lang.substr(-2);
-//       }
-      
-//       function TranslateClearCookie() {
-//         $.cookie('googtrans', null);
-//         $.cookie("googtrans", null, {
-//           domain: "." + document.domain,
-//         });
-//       }
-      
-//       function TranslateSetCookie(code) {
-//         $.cookie('googtrans', "/auto/" + code);
-//         $.cookie("googtrans", "/auto/" + code, {
-//           domain: "." + document.domain,
-//         });
-//       }
-// });
